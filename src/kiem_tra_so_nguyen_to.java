46de82e30b0e5702476386479d8002284efe3a71
ngochung207 <ngochung207@gmail.com>
2020-12-10 09:53:39 +0700

    check so nguyen to

---
+++ src/kiem_tra_so_nguyen_to.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class kiem_tra_so_nguyen_to {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số cần kiểm tra: ");
+        int x = sc.nextInt();
+        boolean check = false;
+        if (x < 2) {
+            check = false;
+        } else {
+            int i = 2;
+            check = true;
+            while (i < Math.sqrt(x)){
+                if (x % i == 0){
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+        }
+        if (check)
+            System.out.println(x + " la so nguyen to");
+        else
+            System.out.println(x + " khong la so nguyen to");
+    }
+}
